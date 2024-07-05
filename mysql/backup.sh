069af7063ac660ae4ac4e4a71b1e4cdce0912a7c
cfd2022 <121007383+cfd2022@users.noreply.github.com>
2024-07-05 16:00:52 +0800

    Update backup.sh

--- mysql/backup.sh
+++ mysql/backup.sh
@@ -66,7 +66,7 @@ XTRABACKUP_CMD="xtrabackup --backup --user=root --password=$DATABASE_PASSWORD --
 
 
 if [ "$MINI_BACKUP_CHOICE" = "y" ]; then
-    XTRABACKUP_CMD+=" --tables-exclude='command_control.data_ebay_search2|command_control.logs_command_control_request_log|command_control.data_ebay_product'"
+    XTRABACKUP_CMD+=" --tables-exclude='command_control.data_ebay_search2|command_control.logs_command_control_request_log|command_control.data_ebay_product|command_control.logs_ebay_product_request_log'"
 else
     XTRABACKUP_CMD+=" --tables-exclude='command_control.data_ebay_search2|command_control.logs_command_control_request_log'"
 fi
